26d67b2392c21b9d59ad7e40880ef036b6d7750b
Stu Doherty <stu.doherty@gmail.com>
2026-04-20 15:30:34 -0400

    feat: add DC2 NLB (ha-postgres-dc2-nlb) with write/read target groups

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -115,3 +115,115 @@ resource "aws_lb_target_group_attachment" "standby_read" {
   target_id        = aws_instance.standbys[count.index].id
   port             = 5001
 }
+
+# --- DC2 Network Load Balancer ---
+
+resource "aws_lb" "ha_postgres_dc2" {
+  count              = var.dc2_enabled ? 1 : 0
+  name               = "ha-postgres-dc2-nlb"
+  internal           = true
+  load_balancer_type = "network"
+  subnets            = local.selected_subnets
+
+  enable_cross_zone_load_balancing = true
+
+  tags = merge(local.common_tags, {
+    Name = "ha-postgres-dc2-nlb"
+  })
+}
+
+resource "aws_lb_target_group" "dc2_pg_write" {
+  count    = var.dc2_enabled ? 1 : 0
+  name     = "ha-postgres-dc2-write-tg"
+  port     = 5000
+  protocol = "TCP"
+  preserve_client_ip = "false"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    protocol            = "HTTP"
+    port                = 8008
+    path                = "/master"
+    interval            = 10
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name = "ha-postgres-dc2-write-tg"
+  }
+}
+
+resource "aws_lb_target_group" "dc2_pg_read" {
+  count    = var.dc2_enabled ? 1 : 0
+  name     = "ha-postgres-dc2-read-tg"
+  port     = 5001
+  protocol = "TCP"
+  preserve_client_ip = "false"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    protocol            = "HTTP"
+    port                = 8008
+    path                = "/replica"
+    interval            = 10
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name = "ha-postgres-dc2-read-tg"
+  }
+}
+
+resource "aws_lb_listener" "dc2_pg_write" {
+  count             = var.dc2_enabled ? 1 : 0
+  load_balancer_arn = aws_lb.ha_postgres_dc2[0].arn
+  port              = 5000
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.dc2_pg_write[0].arn
+  }
+}
+
+resource "aws_lb_listener" "dc2_pg_read" {
+  count             = var.dc2_enabled ? 1 : 0
+  load_balancer_arn = aws_lb.ha_postgres_dc2[0].arn
+  port              = 5001
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.dc2_pg_read[0].arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "dc2_upstream_write" {
+  count            = var.dc2_enabled ? 1 : 0
+  target_group_arn = aws_lb_target_group.dc2_pg_write[0].arn
+  target_id        = aws_instance.dc2_upstream[0].id
+  port             = 5000
+}
+
+resource "aws_lb_target_group_attachment" "dc2_upstream_read" {
+  count            = var.dc2_enabled ? 1 : 0
+  target_group_arn = aws_lb_target_group.dc2_pg_read[0].arn
+  target_id        = aws_instance.dc2_upstream[0].id
+  port             = 5001
+}
+
+resource "aws_lb_target_group_attachment" "dc2_standby_write" {
+  count            = var.dc2_enabled ? 2 : 0
+  target_group_arn = aws_lb_target_group.dc2_pg_write[0].arn
+  target_id        = aws_instance.dc2_standbys[count.index].id
+  port             = 5000
+}
+
+resource "aws_lb_target_group_attachment" "dc2_standby_read" {
+  count            = var.dc2_enabled ? 2 : 0
+  target_group_arn = aws_lb_target_group.dc2_pg_read[0].arn
+  target_id        = aws_instance.dc2_standbys[count.index].id
+  port             = 5001
+}
